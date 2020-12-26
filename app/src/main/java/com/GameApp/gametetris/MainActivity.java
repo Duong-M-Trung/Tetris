34b6e2ea61e85792e7fdbe990c5c2ce60db491a4
Duong-M-Trung <65237331+Duong-M-Trung@users.noreply.github.com>
2020-12-27 00:05:55 +0700

    date 27/12/2020, add background image/add music

--- app/src/main/java/com/GameApp/gametetris/MainActivity.java
+++ app/src/main/java/com/GameApp/gametetris/MainActivity.java
@@ -2,13 +2,17 @@ package com.GameApp.gametetris;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private MediaPlayer mediaPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mediaPlayer = MediaPlayer.create(this,R.raw.tetris_soundtrack);
+        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
         setContentView(R.layout.activity_main);
     }
 }
