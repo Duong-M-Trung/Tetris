dd7f47112e234e799edc46fdf21e855a0a192ca4
Duong-M-Trung <65237331+Duong-M-Trung@users.noreply.github.com>
2020-12-27 20:47:53 +0700

    date 27/12/2020

---
+++ app/src/main/java/com/GameApp/gametetris/Playscreen.java
@@ -0,0 +1,14 @@
+package com.GameApp.gametetris;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Playscreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_playscreen);
+    }
+}
